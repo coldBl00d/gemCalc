cb3e66d75c8a25cf2f203582aeaf83224ab97b7d
Akhil <akhilrajr94@gmail.com>
2015-04-15 11:14:35 +0530

    resourceToGem Done

--- app/src/main/java/com/akhilraj/clashofclangemcalculator/GemCalc.java
+++ app/src/main/java/com/akhilraj/clashofclangemcalculator/GemCalc.java
@@ -29,7 +29,7 @@ public class GemCalc {
             /*
                @Adithya Same stuff that you said but doing the multiplication (timeinSec-plateauTime) I was not getting the result (brackets scene aayi kaanum)
                 So used overall division
-                ***Delete After Read**
+                ***Delete constRester Read**
              */
 
         if ((float)i <= constTime[2])
@@ -38,10 +38,7 @@ public class GemCalc {
         else
             return (int)(((float)i - constTime[2]) / ((constTime[3] - constTime[2]) / (constGem[3] - constGem[2])) + constGem[2]);
     }
-
-}
-
-/*Test Class
+    /*Test Class
    Guys test if wrong
 
 public class Test {
@@ -65,3 +62,44 @@ Output
 
 Correct
  */
+    public int resourceToGem ( float res )
+    {
+        int i=0;
+        float constRes[] = {
+                100F, 1000F, 10000F, 100000F, 1000000F, 10000000F
+        };
+        float constGem[] = {
+                1.0F, 5F, 25F, 125F, 600F, 3000F
+        };
+
+        do
+        {
+            if (i >= -1 + constRes.length) //If the res entered is too large for the storage
+            {
+                if (res <= 8001000F)    // 4 gold collector maxed out storage is 8mil + 1000 from TH
+                {                       // Had to be done separately since 10mil is a plateau value
+                    return (int)(float)Math.round((res - constRes[4]) / ((constRes[5] - constRes[4]) / (constGem[5] - constGem[4])) + constGem[4]);
+                } else
+                {
+                    return (int)0.0F;
+                }
+            }
+            
+            if (res <= constGem[i])
+            {
+                return (int)(float)Math.round((res - constGem[i - 1]) / ((constGem[i] - constGem[i - 1]) / (constRes[i] - constRes[i - 1])) + constRes[i - 1]);
+            }
+            i++;
+
+           
+
+
+        }while (true);
+
+
+    }
+
+
+
+}
+
